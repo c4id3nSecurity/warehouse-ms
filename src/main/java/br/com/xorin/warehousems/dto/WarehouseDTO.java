e8ed5215da4169b123265240805f1a20c528abd0
c4id3nSecurity <leviizinho765@gmail.com>
2023-07-25 12:04:39 -0300

    start of the project

---
+++ src/main/java/br/com/xorin/warehousems/dto/WarehouseDTO.java
@@ -0,0 +1,10 @@
+package br.com.xorin.warehousems.dto;
+
+import lombok.Data;
+
+@Data
+public class WarehouseDTO {
+  private Long productId;
+
+  private int quantity;
+}
--- src/main/java/br/com/xorin/warehousems/model/Warehouse.java
+++ src/main/java/br/com/xorin/warehousems/model/Warehouse.java
@@ -1,4 +1,20 @@
 package br.com.xorin.warehousems.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "tb_warehouse")
+@Data
 public class Warehouse {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "product_id", nullable = false)
+  private Long productId;
+
+  @Column(name = "quantity")
+  private int quantity;
 }
