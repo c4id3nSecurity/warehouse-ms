6b541f5c683fba1784f5b75cdccb3d4cc5b58cd2
c4id3nSecurity <leviizinho765@gmail.com>
2023-07-25 11:57:04 -0300

    start of the project

---
+++ src/main/java/br/com/xorin/warehousems/model/Warehouse.java
@@ -0,0 +1,4 @@
+package br.com.xorin.warehousems.model;
+
+public class Warehouse {
+}
---
+++ src/test/java/br/com/xorin/warehousems/WarehouseMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xorin.warehousems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
